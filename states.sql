c2a73408720f8a612de724cc36d479802de8ed34
samceena <samceen@gmail.com>
2019-10-27 21:11:38 -0400

    Add files via upload

---
+++ states.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 27, 2019 at 09:07 PM
+-- Server version: 5.7.27-0ubuntu0.16.04.1
+-- PHP Version: 7.0.33-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hermesdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+
+CREATE TABLE `states` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `abbreviation` varchar(10) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `states`
+--
+
+INSERT INTO `states` (`id`, `name`, `abbreviation`) VALUES
+(1, 'Alabama', 'AL'),
+(2, 'Alaska', 'AK'),
+(3, 'Arizona', 'AZ'),
+(4, 'Arkansas', 'AR'),
+(5, 'California', 'CA'),
+(6, 'Colorado', 'CO'),
+(7, 'Connecticut', 'CT'),
+(8, 'Delaware', 'DE'),
+(9, 'Florida', 'FL'),
+(10, 'Georgia', 'GA'),
+(11, 'Hawaii', 'HI'),
+(12, 'Idaho', 'ID'),
+(13, 'Illinois', 'IL'),
+(14, 'Indiana', 'IN'),
+(15, 'Iowa', 'IA'),
+(16, 'Kansas', 'KS'),
+(17, 'Kentucky', 'KY'),
+(18, 'Louisiana', 'LA'),
+(19, 'Maine', 'ME'),
+(20, 'Maryland', 'MD'),
+(21, 'Massachusetts', 'MA'),
+(22, 'Michigan', 'MI'),
+(23, 'Minnesota', 'MN'),
+(24, 'Mississippi', 'MS'),
+(25, 'Missouri', 'MO'),
+(26, 'Montana', 'MT'),
+(27, 'Nebraska', 'NE'),
+(28, 'Nevada', 'NV'),
+(29, 'New Hampshire', 'NH'),
+(30, 'New Jersey', 'NJ'),
+(31, 'New Mexico', 'NM'),
+(32, 'New York', 'NY'),
+(33, 'North Carolina', 'NC'),
+(34, 'North Dakota', 'ND'),
+(35, 'Ohio', 'OH'),
+(36, 'Oklahoma', 'OK'),
+(37, 'Oregon', 'OR'),
+(38, 'Pennsylvania', 'PA'),
+(39, 'Rhode Island', 'RI'),
+(40, 'South Carolina', 'SC'),
+(41, 'South Dakota', 'SD'),
+(42, 'Tennessee', 'TN'),
+(43, 'Texas', 'TX'),
+(44, 'Utah', 'UT'),
+(45, 'Vermont', 'VT'),
+(46, 'Virginia', 'VA'),
+(47, 'Washington', 'WA'),
+(48, 'West Virginia', 'WV'),
+(49, 'Wisconsin', 'WI'),
+(50, 'Wyoming', 'WY');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `states`
+--
+ALTER TABLE `states`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `states`
+--
+ALTER TABLE `states`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
